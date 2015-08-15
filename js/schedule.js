7154ca7c1894f33b1cd77fe0bd8111db92499167
Francis <francis.beauchampsc@gmail.com>
2015-08-15 19:53:02 -0400

    Added column refresh when resizing or moving courses

--- js/schedule.js
+++ js/schedule.js
@@ -1,5 +1,7 @@
 (function($, self) {
-	  var elementsInColumn = function(column) {
+  self.active = null;
+
+  var elementsInColumn = function(column) {
     var data = [];
     $(".js-schedule table tr").each(function(rowIndex) {
       $(this).find("td").each(function(cellIndex) {
@@ -11,61 +13,113 @@
     return data;
   };
 
-  var setCourseRows = function(element, from, to) {
-    var column = element.parent().index();
+  // TODO Refactor
+  var elementsInColumnWithCourse = function(column) {
+    var data = [];
+    $(".js-schedule table tr").each(function(rowIndex) {
+      $(this).find("td").each(function(cellIndex) {
+          if (cellIndex == column && $(this).children().length > 0)
+            data.push($(this));
+        });
+    });
+
+    return data;
+  };
+
+  var cellLocation = function(element) {
+    var column = element.index();
     var $tr = element.closest('tr');
     var row = $tr.index();
 
+    return { x: column, y: row };
+  };
+
+  var refreshColumn = function(column) {
     var tdInColumn = elementsInColumn(column);
-    for (var i = 1; i != tdInColumn.length - 1; i++) {
-      if (i > from && i < to)
-        tdInColumn[i].css("display", "none");
+    var rowTaken = 0;
+    var skip = 0;
+    for (var i = 1; i != tdInColumn.length; i++) {
+      if (--skip <= 0) {
+        var rowspan = tdInColumn[i].attr('rowspan');
+        rowTaken = typeof rowspan === 'undefined' || isNaN(rowspan) ? 0 : rowspan;
+        if (tdInColumn[i].children().length != 0 && rowTaken != 0) {
+          tdInColumn[i].css("display", "table-cell");
+          tdInColumn[i].attr('rowspan', rowspan);
+          skip = rowTaken;
+        }
+        else {
+          tdInColumn[i].css("display", "table-cell");
+          tdInColumn[i].attr('rowspan', 1);
+        }
+      }
       else
-        tdInColumn[i].css("display", "table-cell");
+        tdInColumn[i].css("display", "none");
     }
-    console.log('from -> ' + from)
-    console.log('to -> ' + to)
+  }
+
+  var setCourseRows = function(element, from, to) {
+    if (self.active != null) {
+      var fromLocation = cellLocation(self.active);
+    }
+
+    var toLocation = cellLocation(element);
 
     var size = to - from
-    console.log('rows -> ' + size)
-    element.parent().attr('rowspan', size);
+    element.attr('rowspan', size);
+
+    if (self.active != null && fromLocation.x != toLocation.x)
+      refreshColumn(fromLocation.x)
+    
+    refreshColumn(toLocation.x)
   };
 
   var dropInTd = function(event, ui) {
-		var name = '';
-		var id = ui.draggable.data('course');
-		if (ui.draggable.hasClass('schedCourse')) {
-			name = ui.draggable.find('.js-name').html();
-			ui.draggable.remove();
-		}
-		else
-			name = ui.draggable.find('input[type=text]').val()
+    var name = '';
+    var id = ui.draggable.data('course');
+    if (ui.draggable.hasClass('schedCourse')) {
+      name = ui.draggable.find('.js-name').html();
+      ui.draggable.remove();
+    }
+    else
+      name = ui.draggable.find('input[type=text]').val()
 
-		var course = $('<div class="js-course schedCourse" data-course="'+id+'"><b>'+'<div class="js-name">'+name+'</div>'+'</b><input type-"text" placeholder="What ?"><br><input type="text" placeholder="Where ?"><br></div>');
-		
-		course.draggable({ cursorAt: {top: 38, left: 75 }, revert: true });
-		course.resizable({ grid: 61,  handles: 's', 
+    var course = $('<div class="js-course schedCourse" data-course="'+id+'"><b>'+'<div class="js-name">'+name+'</div>'+'</b><input type-"text" placeholder="What ?"><br><input type="text" placeholder="Where ?"><br></div>');
+    
+    if (self.active == null)
+      self.active = $(this);
+
+    course.draggable({ cursorAt: {top: 38, left: 75 }, revert: true, 
+      start: function(event, ui) {
+        self.active = ui.helper.parent();
+      } 
+    });
+
+    course.resizable({ grid: 50,  handles: 's', 
       stop: function(event, ui) {
         var row = ui.element.parent().parent().index();
-        var rowTaken = Math.floor(ui.size.height / 61 + 0.99);
-        setCourseRows(ui.element, row, row + rowTaken);
+        var rowTaken = Math.floor(ui.size.height / 50 + 0.99);
+        setCourseRows(ui.element.parent(), row, row + rowTaken);
       }
     });
 
-		$(this).html(course);
-	};
+    $(this).html(course);
 
-	self.reset = function() {
-		$('td.inner').html(''); // Find all tds and reset them
-		$('.courses ul').html(''); // Find list and clean it
-	};
+    var row = $(this).parent().index();
+    var rowTaken = Math.floor($(this).size.height / 61 + 0.99);
+    setCourseRows($(this), row, row + rowTaken);
+  };
 
-	self.init = function() {
-		$('td.inner').droppable({
-			accept: '.js-course',
-			activeClass: 'ui-state-active',
-			hoverClass: 'ui-state-hover',
-			drop: dropInTd
-		});
-	};
+  self.reset = function() {
+    $('td.inner').html(''); // Find all tds and reset them
+    $('.courses ul').html(''); // Find list and clean it
+  };
+
+  self.init = function() {
+    $('td.inner').droppable({
+      accept: '.js-course',
+      activeClass: 'ui-state-active',
+      hoverClass: 'ui-state-hover',
+      drop: dropInTd
+    });
+  };
 })(jQuery, this.Schedule = { });
